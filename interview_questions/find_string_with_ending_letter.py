292b9aa260b6175ee0adcc32a775221d3e16fbf2
sivaram <sivaram031.md@gmail.com>
2020-12-03 23:50:26 +0530

    add some answer

---
+++ interview_questions/find_string_with_ending_letter.py
@@ -0,0 +1,3 @@
+lst = ['ram','sam','com','siva','jak']
+
+print([x for x in lst if x.lower().endswith('m')])
---
+++ interview_questions/two_list_of_string_find_common_string.py
@@ -0,0 +1,4 @@
+col1 =['red','grenn','yellow','orange']
+col2 =['white','blue','red','orange']
+
+print(set(col1) & set(col2))
