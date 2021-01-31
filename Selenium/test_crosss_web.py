8009c904041d0a3f7878d45e37e5a680529f09ce
sivaram <sivaram031.md@gmail.com>
2021-01-31 20:30:06 +0530

    exit to my pycharm in ubuntu

--- Selenium/test_crosss_web.py
+++ Selenium/test_crosss_web.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium import *
 import time
-
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 chrome = webdriver.Chrome()
 firefox = webdriver.Firefox()
 
@@ -12,6 +13,11 @@ for i in drivers:
     i.get('http://facebook.com\login')
     i.maximize_window()
     '''screenshot methos two types'''
+
+    time.sleep(5)
+    k = ActionChains(i)
+    k.send_keys(Keys.ALT,Keys.TAB).perform()
+    print('sucessfull move tab')
     #i.save_screenshot()
     #i.get_screenshot_as_file()
 
